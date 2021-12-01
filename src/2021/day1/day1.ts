d6de5cba20b4b9707e4185f99a3f371cb0599530
Raz Ali <mehraz.hakim@gmail.com>
2021-12-01 11:18:28 -0500

    2020:day1:problem2 - reimplement threeples function without if/else

--- src/2021/day1/day1.ts
+++ src/2021/day1/day1.ts
@@ -4,14 +4,10 @@ export const parseInput = (input: string): Array<number> =>
     splitLines(input).map(str => Number.parseInt(str))
 
 
-export const threeples = (input: Array<number>): Array<Array<number>> => {
-    return input.reduce((prev, curr, index) => {
-        if (input.length - index > 2) {
-            prev.push(input.slice(index, index + 3))
-        }
-        return prev
-    }, []);
-};
+export const threeples = (input: Array<number>): Array<Array<number>> =>
+    Array(input.length - 2)
+        .fill(null)
+        .map((_, index) => input.slice(index, index + 3));
 
 export const countIncreases = (input: Array<number>): number =>
     input.filter((val, index) => val > input[index - 1]).length
