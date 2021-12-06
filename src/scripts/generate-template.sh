452547b7447b5105e162cb4348c258829e424015
Raz Ali <mehraz.hakim@gmail.com>
2021-12-06 11:47:58 +0400

    add script to generate dirs

---
+++ src/scripts/generate-template.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIR_NAME=$1
+mkdir src/2021/$DIR_NAME src/2021/$DIR_NAME/__tests__
+touch src/2021/$DIR_NAME/__tests__/$DIR_NAME.test.ts
+touch src/2021/$DIR_NAME/data.ts
+touch src/2021/$DIR_NAME/$DIR_NAME.ts
+
