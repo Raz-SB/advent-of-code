273b5ad972cec7d0317010ef827a36368c4f3c4b
Raz Ali <mehraz.hakim@gmail.com>
2021-12-12 21:30:50 +0400

    Add day 10

--- src/scripts/generate-template.sh
+++ src/scripts/generate-template.sh
@@ -5,4 +5,5 @@ mkdir src/2021/$DIR_NAME src/2021/$DIR_NAME/__tests__
 touch src/2021/$DIR_NAME/__tests__/$DIR_NAME.test.ts
 touch src/2021/$DIR_NAME/data.ts
 touch src/2021/$DIR_NAME/$DIR_NAME.ts
+touch src/2021/$DIR_NAME/$DIR_NAME-problem.txt
 
