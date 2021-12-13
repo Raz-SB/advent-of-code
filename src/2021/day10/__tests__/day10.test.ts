5bb1b8848c4cf6e8921803c9e93f93c68eb0e98a
Raz Ali <mehraz.hakim@gmail.com>
2021-12-13 08:45:52 +0400

    day10 complete

--- src/2021/day10/__tests__/day10.test.ts
+++ src/2021/day10/__tests__/day10.test.ts
@@ -1,4 +1,4 @@
-import {scoreSyntaxErrors, validateLine} from "../day10";
+import {completeLine, scoreLineCompletion, scoreSyntaxErrors, solveProblem2, validateLine} from "../day10";
 import exp from "constants";
 import {data} from "../data";
 
@@ -20,6 +20,12 @@ describe('Day 10', function () {
             cases.forEach((testCase) => testValidLine(testCase))
         });
 
+        it('should be valid for incomplete lines', function () {
+            const line = '[({(<(())[]>[[{[]{<()<>>'
+            const validationErrors = validateLine(line)
+            expect(validationErrors).toHaveLength(0)
+        });
+
         describe('finding corrupted lines', function () {
             const cases = ['(]', '{()()()>', '(((()))}', '<([]){()}[{}])'];
             const testInvalidLIne = (testCase: string) => {
@@ -33,9 +39,41 @@ describe('Day 10', function () {
         });
     });
 
-    describe('solving problem 1', function () {
-        describe('for small data set', function () {
-            const testData = `[({(<(())[]>[[{[]{<()<>>
+    describe('completing lines', function () {
+        /**
+
+         */
+        const cases = [
+            {line: '[({(<(())[]>[[{[]{<()<>>', completion: '}}]])})]'},
+            {line: '[(()[<>])]({[<{<<[]>>(', completion: ')}>]})'},
+            {line: '(((({<>}<{<{<>}{[]{[]{}', completion: '}}>}>))))'},
+            {line: '{<[[]]>}<{[{[{[]{()[[[]', completion: ']]}}]}]}>'},
+            {line: '<{([{{}}[<[[[<>{}]]]>[]]', completion: '])}>'}
+        ]
+
+        const testCompletion = (testCase: { line: string, completion: string }) => {
+            it(`should complete ${testCase.line} with ${testCase.completion}`, function () {
+                const incompleteLine = testCase.line
+                const expectedResult = testCase.completion
+                const actualResult = completeLine(incompleteLine)
+                expect(actualResult).toEqual(expectedResult)
+            });
+        }
+
+        cases.forEach(testCompletion)
+
+        describe('scoring', function () {
+            it('should score correctly', function () {
+                const expectedScore = 294
+                const actualScore = scoreLineCompletion('])}>')
+                expect(actualScore).toEqual(expectedScore)
+            });
+
+        });
+    });
+
+    describe('problems', function () {
+        const testData = `[({(<(())[]>[[{[]{<()<>>
 [(()[<>])]({[<{<<[]>>(
 {([(<{}[<>[]}>{[]{[(<()>
 (((({<>}<{<{<>}{[]{[]{}
@@ -46,17 +84,37 @@ describe('Day 10', function () {
 <{([([[(<>()){}]>(<<{{
 <{([{{}}[<[[[<>{}]]]>[]]`
 
-            it('should score invalid chars for small dataset', function () {
-                const expectedResult = 26397
-                const actualScore = scoreSyntaxErrors(testData)
-                expect(actualScore).toEqual(expectedResult)
+        describe('solving problem 1', function () {
+            describe('for small data set', function () {
+                it('should score invalid chars for small dataset', function () {
+                    const expectedResult = 26397
+                    const actualScore = scoreSyntaxErrors(testData)
+                    expect(actualScore).toEqual(expectedResult)
+                });
+
+                it('should score for large dataset', function () {
+                    const expectedResult = 168417
+                    const actualScore = scoreSyntaxErrors(data)
+                    expect(actualScore).toEqual(expectedResult)
+                });
+            });
+        });
+
+        describe('solving problem 2', function () {
+            describe('discard corrupted lines, complete incomplete lines, score them, sort them, and return middle score', function () {
+                it('should work for small dataset', function () {
+                    const expectedResult = 288957
+                    const actualResult = solveProblem2(testData)
+                    expect(actualResult).toEqual(expectedResult)
+                });
+
+                it('should work for large dataset', function () {
+                    const expectedResult = 288957
+                    const actualResult = solveProblem2(data)
+                    expect(actualResult).toEqual(expectedResult)
+                });
             });
 
-            it('should score for large dataset', function () {
-                const expectedResult = 168417
-                const actualScore = scoreSyntaxErrors(data)
-                expect(actualScore).toEqual(expectedResult)
-            });
         });
     });
 });
--- src/2021/day10/day10.ts
+++ src/2021/day10/day10.ts
@@ -34,9 +34,45 @@ export const validateLine = (line: string) => {
     return []
 }
 
+const lineCompletionScore = {
+     ')': 1,
+     ']': 2,
+     '}': 3,
+     '>': 4,
+}
+
+export const scoreLineCompletion = (line: string):number =>  {
+   return line.split('')
+       .reduce((prev, curr) => {
+           return (prev * 5) + lineCompletionScore[curr]
+       }, 0)
+}
+
+export const completeLine = (line: string): string => {
+    const expectedClosingChars = []
+    for (let char of line.split('')) {
+        if (char in charSets) {
+            expectedClosingChars.push(charSets[char])
+        }  else if (char !== expectedClosingChars.pop()) {
+            throw new Error('corrupted line')
+        }
+    }
+    return expectedClosingChars.reverse().join('')
+}
+
 export const scoreSyntaxErrors = (input: string): number => {
     return sum(splitLines(input)
         .map(eachLine => validateLine(eachLine))
         .reduce((prev, curr) => [...prev, ...curr], [])
         .map(eachIllegalChar => scores[eachIllegalChar]))
+}
+
+export const solveProblem2 = (input: string): number => {
+    const sortedScores = splitLines(input)
+        .filter(line => validateLine(line).length === 0)
+        .map(incompleteLines => completeLine(incompleteLines))
+        .map(completions => scoreLineCompletion(completions))
+        .sort((a, b) => a - b)
+
+    return sortedScores[Math.floor(sortedScores.length / 2)];
 }
