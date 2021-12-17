3745ffdd361185aa78318b4ad970bbbbd8966d65
Raz Ali <mehraz.hakim@gmail.com>
2021-12-17 09:05:31 -0500

    completed day 12

---
+++ src/2021/day12/__tests__/day12.test.ts
@@ -0,0 +1,61 @@
+import {Cave, Path, findPathBetween, parseInput, findDistinctPaths, findDistinctPaths2} from "../day12";
+import exp from "constants";
+import {data} from "../data";
+
+
+describe('Day 12', function () {
+    const testData = `start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end`
+
+    describe('caves', function () {
+        it('should be big cave if identifier is uppercase', function () {
+            const bigCave = new Cave('AB')
+            expect(bigCave.isBig).toBeTruthy()
+        });
+
+        it('should be small cave if identifier is lowercase', function () {
+            const bigCave = new Cave('ab')
+            expect(bigCave.isBig).toBeFalsy()
+        });
+
+    });
+
+    describe('parsing input', function () {
+
+        it('should parse input', function () {
+            const paths = parseInput(testData)
+            expect(paths).toHaveLength(7)
+        });
+    });
+
+    describe('problem 1', function () {
+        describe('find all distinct paths', function () {
+            it('should return all paths', function () {
+                const paths = findDistinctPaths(parseInput(testData), 'start', 'end')
+                expect(paths).toEqual(10)
+            });
+            it('should work for large input', function () {
+                const paths = findDistinctPaths(parseInput(data), 'start', 'end')
+                expect(paths).toEqual(4549)
+            });
+        });
+    });
+
+    describe('problem 2', function () {
+        describe('find all distinct paths', function () {
+            it('should return all paths', function () {
+                const paths = findDistinctPaths2(parseInput(testData), 'start', 'end')
+                expect(paths.length).toEqual(36)
+            });
+            it('should work for large input', function () {
+                const paths = findDistinctPaths2(parseInput(data), 'start', 'end')
+                expect(paths.length).toEqual(10)
+            });
+        });
+    });
+});
--- src/2021/day12/day12.ts
+++ src/2021/day12/day12.ts
@@ -1,21 +1,99 @@
-export const data = `ma-start
-YZ-rv
-MP-rv
-vc-MP
-QD-kj
-rv-kj
-ma-rv
-YZ-zd
-UB-rv
-MP-xe
-start-MP
-zd-end
-ma-UB
-ma-MP
-UB-xe
-end-UB
-ju-MP
-ma-xe
-zd-UB
-start-xe
-YZ-end`
+import {split} from "lodash";
+import {splitLines} from "../../utils/dataReader";
+
+export class Cave {
+    private _isBigCave: boolean;
+
+    constructor(public identifier: string) {
+        this._isBigCave = identifier.toUpperCase() === identifier
+    }
+
+    public get isBig() {
+        return this._isBigCave
+    }
+}
+
+export class Path {
+    public caves: Cave[]
+
+    constructor(...caves: Cave[]) {
+        this.caves = caves
+    }
+}
+
+const isBig = (identifier: string) => {
+    return identifier.toUpperCase() === identifier
+}
+
+const isUnvisited = (currentPath: string[], connectingCaves: string): boolean => !currentPath.includes(connectingCaves);
+
+const canVisit = (currentPath: string[], potentialCaveToVisit: string): boolean => {
+    const smallCavesPreviouslyVisited = currentPath
+        .filter(x => !isBig(x))
+        .filter(x => x !== 'start' && x !== 'end')
+        .reduce((prev, curr) => {
+            prev[curr] = (prev[curr] || 0) + 1
+            return prev
+        }, {})
+
+    let maxNumberOfVisitsToSmallCaves = Math.max(...Object.values(smallCavesPreviouslyVisited).map(x => x as number));
+    let canVisit = potentialCaveToVisit !== 'start' &&
+        (maxNumberOfVisitsToSmallCaves < 2
+            || (maxNumberOfVisitsToSmallCaves == 2 && !smallCavesPreviouslyVisited[potentialCaveToVisit]))
+
+    return canVisit
+};
+
+export const parseInput = (input: string): Path[] => {
+    return splitLines(input)
+        .map(eachLine => eachLine.split('-').map(x => new Cave(x)))
+        .reduce((prev, [caveA, caveB]) => [...prev, new Path(caveA, caveB)], [] as Path[]);
+}
+
+export const findDistinctPaths = (connections: Path[], startCave: string, endCave: string) => {
+    const allPaths: string[][] = [[]]
+    const buildPaths = (currentPath: string[], startCave: string) => {
+        // allPaths.forEach(existingPath => existingPath.push(startCave))
+        const newPath = [...currentPath, startCave]
+        allPaths.push(newPath)
+
+        const cavesFromStarCave = connections.filter(conn => conn.caves.some(cave => cave.identifier === startCave))
+            .map(conn => conn.caves.find(c => c.identifier !== startCave).identifier)
+            .filter(connectingCaves => isBig(connectingCaves) || isUnvisited(newPath, connectingCaves))
+
+        cavesFromStarCave.forEach(connCave => {
+            // allPaths.forEach(existingPath => existingPath.push(connCave))
+            buildPaths(newPath, connCave)
+        })
+    }
+
+    buildPaths([], startCave)
+
+    let fullPaths = allPaths.filter(path => path.pop() === endCave);
+    return fullPaths.length;
+};
+
+export const findDistinctPaths2 = (connections: Path[], startCave: string, endCave: string) => {
+    const allPaths: string[][] = [[]]
+    const buildPaths = (currentPath: string[], startCave: string) => {
+        // allPaths.forEach(existingPath => existingPath.push(startCave))
+        const newPath = [...currentPath, startCave]
+        allPaths.push(newPath)
+
+        if (startCave !== endCave) {
+            const cavesFromStarCave = connections.filter(conn => conn.caves.some(cave => cave.identifier === startCave))
+                .map(conn => conn.caves.find(c => c.identifier !== startCave).identifier)
+                .filter(connectingCaves => isBig(connectingCaves) || canVisit(newPath, connectingCaves))
+
+            cavesFromStarCave.forEach(connCave => {
+                // allPaths.forEach(existingPath => existingPath.push(connCave))
+                buildPaths(newPath, connCave)
+            })
+        }
+    }
+    buildPaths([], startCave)
+    let fullPaths = allPaths.filter(path => path.pop() === endCave);
+    console.log(fullPaths.join(',end\n'))
+    return fullPaths;
+};
+
