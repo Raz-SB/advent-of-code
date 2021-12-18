112d6fe5de548f91cf6df17b996bd108f3966c70
Raz Ali <mehraz.hakim@gmail.com>
2021-12-18 10:01:59 -0500

    solved day 13 but output is mirrored

--- src/2021/day13/__tests__/day13.test.ts
+++ src/2021/day13/__tests__/day13.test.ts
@@ -1,5 +1,6 @@
-import {toGrid, toPoints} from "../day13";
+import {countDots, foldLeft, foldUp, solveProblem2, toGrid, toPoints} from "../day13";
 import {Point} from "../../../utils/grid";
+import {data} from "../data";
 
 describe('Day 13', function () {
     const testData = `6,10
@@ -31,11 +32,52 @@ describe('Day 13', function () {
     describe('toGrid', function () {
         it('should create grid from coordinates', function () {
             const result = toGrid(toPoints(testData))
-            result.print(' ')
+            result.print('  ')
             expect(result.rows).toHaveLength(15)
             expect(result.columns).toHaveLength(11)
         });
 
     });
 
+    describe('folding', function () {
+        it('should fold up', function () {
+            const grid = toGrid(toPoints(testData))
+            const result = foldUp(grid, 7)
+            result.print(' ')
+            const numberOfDots = countDots(result)
+            expect(numberOfDots).toEqual(17)
+        });
+
+        it('should fold left', function () {
+            const grid = toGrid(toPoints(testData))
+            const foldUpResult = foldUp(grid, 7)
+            const foldLeftResult = foldLeft(foldUpResult, 5)
+            foldLeftResult.print(' ')
+            const numberOfDots = countDots(foldLeftResult)
+            expect(numberOfDots).toEqual(16)
+        });
+
+    });
+
+
+    describe('problem 1', function () {
+        it('should solve for large dataset', function () {
+            const grid = toGrid(toPoints(data))
+            const result = foldLeft(grid, 655)
+            result.print(' ')
+            const numberOfDots = countDots(result)
+            expect(numberOfDots).toEqual(17)
+        });
+    });
+
+    describe('problem 2', function () {
+        it('should perform folds and print result', function () {
+            const grid = solveProblem2(data)
+            grid.print('  ')
+            const numberOfDots = countDots(grid)
+            expect(numberOfDots).toEqual(17)
+        });
+
+    });
 });
+
--- src/2021/day13/day13.ts
+++ src/2021/day13/day13.ts
@@ -5,19 +5,83 @@ import {fillArray} from "../../utils/listOps";
 type GridContents = '#' | '.'
 export const toPoints = (input: string) => {
     let coords = splitLines(input).map(str => {
-        const [y, x] = str.split(',').map(each => Number.parseInt(each))
-        return new Point(x, y)
-    });
+        if (!str.startsWith('fold')) {
+            const [y, x] = str.split(',').map(each => Number.parseInt(each))
+            return new Point(x, y)
+        }
+    })
+        .filter(x => !!x);
     return coords
 }
 
 const dimensions = (points: Point[]): Point => new Point(Math.max(...points.map(pt => pt.row)) + 1,
     Math.max(...points.map(pt => pt.column)) + 1)
 
-export const toGrid =  (points: Point[]): Grid<GridContents> => {
+export const toGrid = (points: Point[]): Grid<GridContents> => {
     const dims = dimensions(points)
     let gridRows = fillArray(dims.row).map(() => fillArray(dims.column).map(() => '.' as '.'));
     let grid = new Grid<GridContents>(gridRows);
     points.forEach(pt => grid.setCell(pt, '#'))
     return grid
+}
+
+export function foldUp(grid: Grid<GridContents>, foldPoint: number): Grid<GridContents> {
+    let topHalf = grid.rows.slice(0, foldPoint);
+    let bottomHalf = grid.rows.slice(foldPoint + 1);
+    const gridFromTopHalf = new Grid(topHalf)
+    const linesInTopHalf = gridFromTopHalf.rows.length;
+
+    bottomHalf.forEach((row, bottomRowIndex) => row.forEach((cell, columnIndex) => {
+        if (cell === '#') {
+            gridFromTopHalf.setCell(new Point(linesInTopHalf - 1 - bottomRowIndex, columnIndex), '#')
+        }
+    }))
+
+    return gridFromTopHalf
+}
+
+export function foldLeft(grid: Grid<GridContents>, foldPoint: number): Grid<GridContents> {
+    let leftHalf = grid.rows.map(eachRow => eachRow.slice(0, foldPoint))
+    let rightHalf = grid.rows.map(eachRow => eachRow.slice(foldPoint + 1))
+    const gridFromRightHalf = new Grid(rightHalf)
+    const columnsInLeftHalf = leftHalf[0].length;
+
+    leftHalf.forEach((row, leftRowIndex) => row.forEach((cell, columnIndex) => {
+        if (cell === '#') {
+            gridFromRightHalf.setCell(new Point(leftRowIndex, columnsInLeftHalf - 1 - columnIndex), '#')
+        }
+    }))
+
+    return gridFromRightHalf
+}
+
+export const countDots = (grid: Grid<GridContents>): number => {
+    return grid.allCells.reduce((prev, curr) => curr === '#' ? prev + 1 : prev, 0)
+}
+
+export const parseFoldInstructions = (input: string): { axis: 'x' | 'y', foldPoint: number }[] => {
+    return splitLines(input)
+        .filter(line => line.startsWith('fold'))
+        .map(eachLine => {
+            const [foldInstructions] = eachLine.split(' ').reverse()
+            const [axis, foldPoint] = foldInstructions.split('=')
+            return {axis: axis as 'x' | 'y', foldPoint: Number.parseInt(foldPoint)}
+        })
+
+}
+
+export const solveProblem2 = (input: string): Grid<GridContents> => {
+    let grid = toGrid(toPoints(input))
+    const foldInstructions = parseFoldInstructions(input)
+
+    foldInstructions.forEach(instruction => {
+        if (instruction.axis === 'x') {
+            grid = foldLeft(grid, instruction.foldPoint)
+        }
+        if (instruction.axis === 'y') {
+            grid = foldUp(grid, instruction.foldPoint)
+        }
+    })
+
+    return grid;
 }
