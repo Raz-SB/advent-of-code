c74c043488b65f410c54d68bea480e67434f4e79
raz <mehraz.hakim@gmail.com>
2022-12-09 18:53:23 -0500

    Add fetch script to fetch input from server when creating directory for new day

--- scripts/directory-setup.ts
+++ scripts/directory-setup.ts
@@ -1,16 +1,22 @@
 import fs from "fs";
 import tree from 'tree-node-cli';
+import {fetchInput} from "./fetch-input";
 
-export const setupNewDay = (basePath: string, year: string, day: string) => {
-    const path = `${basePath}/${year}/${day}`;
-    const testsPath = `${basePath}/${year}/${day}/__test__`;
-    if (!fs.existsSync(testsPath)){
-        fs.mkdirSync(testsPath, { recursive: true });
+export const setupNewDay = async (basePath: string, year: string, day: string) => {
+    const path = `${basePath}/${year}/day${day}`;
+    const testsPath = `${basePath}/${year}/day${day}/__test__`;
+    if (!fs.existsSync(testsPath)) {
+        fs.mkdirSync(testsPath, {recursive: true});
     }
 
     fs.writeFileSync(`${path}/problemSolver.ts`, '');
-    fs.writeFileSync(`${path}/data.ts`, '');
+
+    const sessionToken = '' // get your session token from the browser
+    fetchInput(year, parseInt(day).toString(), sessionToken)
+        .then(input => {
+            fs.writeFileSync(`${path}/data.ts`, `export const data = \`${input}\``)
+        })
     fs.writeFileSync(`${testsPath}/problemSolver.test.ts`, '');
-    const fileTree = tree(path, { trailingSlash: true });
+    const fileTree = tree(path, {trailingSlash: true});
     console.log(fileTree);
 }
